bcb03e197c1739b30648012b789d47aa896981e0
aki2228824 <sea.2228824@gmail.com>
2020-07-05 14:59:39 +0900

    '変数の再代入'

---
+++ chapter1/Main1_3.java
@@ -0,0 +1,9 @@
+public class Main1_3 {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("私の年齢は" + age);
+        age = 31;
+        System.out.println("・・・いや本当の年齢は" + age);
+    }
+}
+
