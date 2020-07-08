c2335f9a6966450cb7821bd72530fe08c590791c
aki2228824 <sea.2228824@gmail.com>
2020-07-08 15:40:09 +0900

    calcapp.logics.CalcLogicをインポートする

---
+++ chapter6/Calc6_8.java
@@ -0,0 +1,11 @@
+package clacapp.main;
+import calcapp.logics.CalcLogic;
+
+public class Calc6_8 {
+    public static void main(String[] args) {
+
+        int total = CalcLogic.tasu(a, b);
+        int delta = calcapp.logics.CalcLogic.hiku(a, b);
+        
+    }
+}
