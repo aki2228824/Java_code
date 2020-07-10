ffa9ebcb80a4beca7ee1f26f6fb01139cc3ea2f4
aki2228824 <sea.2228824@gmail.com>
2020-07-10 22:45:55 +0900

    claccTriangleArea

---
+++ chapter5/practice/Main5_4.java
@@ -0,0 +1,12 @@
+public class Main5_4 {
+    public static double calcTriangleArea(double bottom, double height) {
+        return (bottom * height) / 2 ;
+        }
+    public static double calcCircleArea(double radius) {
+        return radius * radius * 3.14;
+    }
+    public static void main(String[] args) {
+        System.out.println("三角形の面積は" + calcTriangleArea(10.0, 5.0) + "cm2");
+        System.out.println("円の面積は" + calcCircleArea(5.0) + "cm2");
+    }
+}
