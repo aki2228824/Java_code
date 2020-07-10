33a06ea3479e0b380d76f73fcb112a4834304314
aki2228824 <sea.2228824@gmail.com>
2020-07-10 22:42:02 +0900

    introduceOneself

---
+++ chapter5/practice/Main5_1.java
@@ -0,0 +1,12 @@
+public class Main5_1 {
+    public static void main(String[] args) {
+      introduceOneself();
+    }
+    public static void introduceOneself() {
+        String name = "aki";
+        int age = 34;
+        double height = 174.0;
+        char eto = '寅';
+        System.out.println("私の名前は" + name + "です。" + "歳は" + age + "歳です。" + "身長は" + height + "cmです。" + "干支は"+ eto + "です。");
+    }
+}
