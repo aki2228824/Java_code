45eef7f6a51841557efa5e88f36f684cae2941ba
aki2228824 <sea.2228824@gmail.com>
2020-07-18 11:40:03 +0900

    mainメソッドを作る

---
+++ chapter8/Main8_1.java
@@ -0,0 +1,9 @@
+public class Main8_1 {
+    public static void main(String[] args) {
+        /*以下の内容をJavaで記述していく
+        　勇者よ、この仮想世界に生まれろ！
+        　お化けきのこよ、この仮想世界に生まれろ！
+        　勇者よ、戦え！
+        　お化けきのこよ、逃げろ！*/
+    }
+}
