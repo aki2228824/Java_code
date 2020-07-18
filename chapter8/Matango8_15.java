c2d86e801053e3d0d1800cf86576edf05bfab588
aki2228824 <sea.2228824@gmail.com>
2020-07-18 12:26:09 +0900

    お化けきのこクラスの定義

---
+++ chapter8/Matango8_15.java
@@ -0,0 +1,8 @@
+public class Matango8_15 {
+    int hp;
+    final int LEVEL = 10;
+    char suffix;
+    public void run() {
+        System.out.println("お化けきのこ" + this.suffix + "逃げ出した！");
+    }
+}
