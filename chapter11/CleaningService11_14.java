bb57a46a5ba315e44289d956cac97f1e1b3cfec7
aki2228824 <sea.2228824@gmail.com>
2020-07-19 21:57:26 +0900

    CleaningServiceインタフェースを継承したクラスの定義

---
+++ chapter11/CleaningService11_14.java
@@ -0,0 +1,5 @@
+public interface CleaningService11_14 {
+    Shirt washShirt(Shirt s);
+    Towl washTowl(Towl t);
+    Coat washCoat(coat c);
+}
---
+++ chapter11/KyotoCleaningShop11_15.java
@@ -0,0 +1,17 @@
+public class KyotoCleaningShop11_15 implements CleaningService11_14 {
+    String ownerName;
+    String address;
+    String phone;
+
+    public Shirt washShirt(Shirt s) {
+        return s;
+    }
+
+    public Towl washTowl(Towl t) {
+        return t;        
+    }
+
+    public Coat washCoat(Coat c) {
+        return c;
+    }
+}
