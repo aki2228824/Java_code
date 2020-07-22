3fa563db27232370d355638ea3438414c147f131
aki2228824 <sea.2228824@gmail.com>
2020-07-22 11:12:06 +0900

    カプセル化を行いsetterメソッドにて値の妥当性をチェック

---
+++ chapter13/Hero13_12.java
@@ -0,0 +1,9 @@
+public class Hero13_12 {
+    private String name;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+      this.nema = name;
+    }
+}
