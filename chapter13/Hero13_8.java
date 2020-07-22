35846aae7f1d5eafbb62f850309af56567a1289f
aki2228824 <sea.2228824@gmail.com>
2020-07-22 10:55:16 +0900

    getNameメソッド、呼び出しを追加

---
+++ chapter13/Hero13_8.java
@@ -0,0 +1,7 @@
+public class Hero13_8 {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ chapter13/King13_9.java
@@ -0,0 +1,6 @@
+public class King13_9 {
+    void talk(Hero13_8 h) {
+        System.out.println("ようこそわが王国へ、勇者" + h.getName() + "よ");
+
+    }
+}
