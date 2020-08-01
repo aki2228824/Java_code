8465bbebcca1cc576d1b39a97cd36000c6c7c4d3
aki2228824 <sea.2228824@gmail.com>
2020-08-01 13:57:07 +0900

    何も定義していないクラスでtoString()を呼ぶ

---
+++ chapter14/Main14_1.java
@@ -0,0 +1,7 @@
+public class Main14_1{
+    public static void main(String[] args) {
+        Empty14_1 e = new Empty14_1();
+        String s = e.toString();
+        System.out.println(s);
+    }
+}
