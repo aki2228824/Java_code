9dafec2062125305150ba7e5e6650d43489199f1
aki2228824 <sea.2228824@gmail.com>
2020-08-02 21:42:27 +0900

    文字列調査メソッドを利用した例

---
+++ chapter15/Main15_1.java
@@ -0,0 +1,18 @@
+public class Main15_1 {
+    public static void main(String[] args) {
+        String s1 = "スッキリJava";
+        String s2 = "Java";
+        String s3 = "java";
+        if (s2.equals(s3)){
+            System.out.println("s2とs3は等しい");
+        }
+        if(s2.equalsIgnoreCase(s3)) {
+            System.out.println("s2とs3は区別しなければ等しい");
+        }
+        System.out.println("s1のながさは" + s1.length() + "です");
+
+        if(s1.isEmpty()){
+            System.out.println("s1は空文字です");
+        }
+    }
+}
