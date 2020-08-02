4555798b82ed86657625cb7cf60253c28395f38a
aki2228824 <sea.2228824@gmail.com>
2020-08-02 22:16:32 +0900

    StringBuilderを利用し文字列を10回連結して表示する

---
+++ chapter15/Main15_4.java
@@ -0,0 +1,9 @@
+public class Main15_4 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 10; i++) {
+            sb.append("Java");
+        }
+        System.out.println(sb.toString());
+    }
+}
