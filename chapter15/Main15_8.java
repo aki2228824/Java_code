f5321d3f0c31f5d52dc9ec5e52945c1cdaf791e1
aki2228824 <sea.2228824@gmail.com>
2020-08-03 17:58:21 +0900

    replaceAllメソッドを使った文字列の置換

---
+++ chapter15/Main15_8.java
@@ -0,0 +1,7 @@
+public class Main15_8 {
+    public static void main(String[] args) {
+        String s = "abs,def:ghi";
+        String w = s.replaceAll("[beh]","X");
+        System.out.println(w);
+    }
+}
