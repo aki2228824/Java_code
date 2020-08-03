4255953592ea9411bb06d292b5c9870841d03a0d
aki2228824 <sea.2228824@gmail.com>
2020-08-03 17:46:04 +0900

    文字列をメソッド、パターンを用いてチェック

---
+++ chapter15/Main15_6.java
@@ -0,0 +1,7 @@
+public class Main15_6 {
+    public static void main(String[] args) {
+    }
+    public boolean isValidPlayerName(String name) {
+        return name.matches("[A-Z][A-Z0-9]{7}");
+    }
+}
